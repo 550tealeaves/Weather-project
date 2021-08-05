ec34112331e2fa92ff22a0c0bd4ae00c848e060b
nclaire29 <nastasia.claire89@gmail.com>
2021-08-04 22:06:59 -0400

    Revised html, css, js code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//Step 1 - build the part that shows current date/time
 let now = new Date(); //(1) must use the new Date variable to get current date/time 
-let dayTime = document.querySelector("#day-time"); //(2) select element w/ id "day-time" - this is where time will display
+let dayTime = document.querySelector("#date"); //(2) select element w/ id "day-time" - this is where time will display
 let hour = now.getHours(); //(3) get hour variable
 let minute = now.getMinutes(); //(4) get minute variable 
 let year = now.getFullYear();
@@ -31,44 +30,61 @@ let months = [ //created month arrays
     "December"
 ];
 let month = months[now.getMonth()];
-dayTime.innerHTML = `${day}, ${month} ${date}, ${year} ${hour}:${minute}`; //(7) use new variables to modify the HTML to show the day and time
 
-//Step 2 - build the search engine that lets you type in a city name and it appears in the 1
-function showCitySearch(event) { //(3) create function that will be called by eventListener
-    event.preventDefault(); //(4) prevent default action of form reloading page
-    let showCity = document.querySelector("#search-city-text"); //(5) step 3 is targeting the element input
-    let h1 = document.querySelector("h1"); //(6) select h1 to modify the element
-    if (showCity.value.length > 0) {  //(7) entry is valid if it has at least 1 character
-        h1.innerHTML = `${showCity.value}`; //(8) display the entry from step 7 in the h1
-    }
+
+dayTime.innerHTML = `${day}, ${month} ${date}, ${year} ${hour}:${minute}`;
+
+
+function displayWeatherCondition(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+
+
+    //Step 7 - target icon to change along w/ weather description
+
+    celsiusTemperature = response.data.main.temp; //STEP 27 - set celsiusTemperature variable = to path w/ Celsius temp in API
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    cityElement.innerHTML = response.data.name; //this is where city name is shown in object 
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
-let citySearch = document.querySelector("#city-search"); //(1) select form
-citySearch.addEventListener("submit", showCitySearch); //(2) add eventListener via click that will call a function showCitySearch
 
-//Step 3 - build the temperature API 
-let apiKey = "ab6da5069e5bc23122a387b3e99bd05b";
-let city = "Havana";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-function showTemp(response) {
-    let newTemp = Math.round(response.data.main.temp);
-    let changingTemp = document.querySelector("#temperature");
-    changingTemp.innerHTML = `${newTemp}`;
-    console.log(response.data);
+function searchCity(city) {
+    let apiKey = "ab6da5069e5bc23122a387b3e99bd05b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeatherCondition);
 }
-let searchCity = document.querySelector("#search-city-button");
-searchCity.addEventListener("submit", showTemp);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
 
-//Step 4 - build the Geolocation API for the current tab
-function currentWeather(event) {
+function handleSubmit(event) {
     event.preventDefault();
-    let locationTemperature = Math.round(22.7);
-    let weatherHere = document.querySelector("#temperature");
-    weatherHere.innerHTML = `${locationTemperature}`;
+    let city = document.querySelector("#city-input").value;
+    searchCity(city);
 }
-function findLocation(position) {
-    console.log(position);
-    let myPosition = document.querySelector("#current-location-button");
-    myPosition.addEventListener("click", currentWeather);
+
+function searchLocation(position) {
+    let apiKey = "ab6da5069e5bc23122a387b3e99bd05b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude
+        }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeatherCondition);
 }
-navigator.geolocation.getCurrentPosition(findLocation);
+
+function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let celsiusTemperature = null;
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
